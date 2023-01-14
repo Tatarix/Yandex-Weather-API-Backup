40f51455a6a5572ed7eb657baaef95aad09a9632
Aleksandr <aleksandrmagiyarov@gmail.com>
2023-01-14 15:44:04 +0300

    Added unloading by the coordinates of two airports (you can add as many points as you like as part of API requests)

--- YandexAPI.py
+++ YandexAPI.py
@@ -5,7 +5,23 @@ import config  #Необходимо получить токен для свое
 def vko():
     headers = config.token
     params = {
-        'lat': '55.605212', 'lon': '37.286370', 'lang': 'ru_RU'
+        'lat': '55.605212', 'lon': '37.286370', 'lang': 'ru_RU',
+    }
+    # в переменнуй params забивай все нужные параметры по городу из раздела объект fact документации
+    url = 'https://api.weather.yandex.ru/v2/informers?'
+    responce = requests.get(url=url, params=params, headers=headers)
+    json_res = responce.json()
+    fact = json_res['fact']
+    # в переменной fact я получил полностью только второй словать из ответа,
+    # где значения fact (см документацию, что там есть в этом словаре)
+    data = {'Температура': fact['temp'], 'Скорость ветра': fact['wind_speed'], 'Погодные условия': fact['condition']}
+    return data
+
+
+def dme():
+    headers = config.token
+    params = {
+        'lat': '55.415984', 'lon': '37.901778', 'lang': 'ru_RU'
     }
     # в переменнуй params забивай все нужные параметры по городу из раздела объект fact документации
     url = 'https://api.weather.yandex.ru/v2/informers?'
@@ -19,15 +35,18 @@ def vko():
 
 
 res_vko = list(vko().values())
-
-
+res_dme = list(dme().values())
 # То,что ниже тут происходит преобразование в ДФ и выгрузка в эксель с переименовыванием столбцов в выгрузке
 def result():
     df_vko = pd.DataFrame(res_vko).transpose()
     df_vko.columns = ['Температура', 'Скорость ветра', 'Погодные условия']
     df_vko.index = ['Внуково']
-    writer = pd.ExcelWriter('test18.xlsx')
-    df_vko.to_excel(writer)
+    df_dme = pd.DataFrame(res_dme).transpose()
+    df_dme.columns = ['Температура', 'Скорость ветра', 'Погодные условия']
+    df_dme.index = ['Домодедово']
+    all_airports = pd.concat([df_vko, df_dme])
+    writer = pd.ExcelWriter('test24.xlsx')
+    all_airports.to_excel(writer)
     return writer.save()
 result()
 
